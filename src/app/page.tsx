0372653cb8573649caae0df503b7a34e4ebddab9
murad <vvanhellsing30@gmail.com>
2025-04-18 23:18:34 +0600

    create all components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Introduction from '../components/Introduction'
+import Skills from '../components/Skills'
+import Footer from '../components/Footer'
+import Projects from '../components/Projects'
+import About from '../components/About'
+import Contact from '../components/Contact'
 
 const page = () => {
   return (
     <div className='min-h-screen'>
       <Navbar/>
-      
+      <Introduction></Introduction>
+      <Skills></Skills>
+      <Projects></Projects>
+      <About></About>
+      <Contact></Contact>
+      <Footer></Footer>
     </div>
   )
 }
