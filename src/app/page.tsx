047d633313e2b0a528e42b6fc0e60f369ce10108
Md Nifad Uzzaman <nifaduzzaman2005@gmail.com>
2025-04-18 23:08:07 +0600

    Nav created -nifad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const page = () => {
   return (
     <div className='min-h-screen'>
+      <Navbar/>
       
-      nifad is writing this code
     </div>
   )
 }
