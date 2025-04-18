587c2b7ee9320cce4a1d648f2afb2f4f1d283bbe
Md Nifad Uzzaman <nifaduzzaman2005@gmail.com>
2025-04-19 00:33:17 +0600

    Intro

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
-import { ourData } from '../libs/data';
+import React from "react";
+import { ourData } from "../libs/data";
+import Image from "next/image";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import Link from "next/link";
 
 const Introduction = () => {
-    console.log(ourData);
-    return (
-        <div>
-            
-        </div>
-    );
+  console.log(ourData);
+  return (
+    <div className="w-full min-h-96 ">
+      <div className="w-full md:w-[80%] gap-2 min-h-96 md:flex   mx-auto">
+        {Array(ourData.length)
+          .fill(0)
+          .map((_, index) => (
+            <div key={index} className="min-h-96 flex-col flex md:w-[50%]  backdrop-blur-3xl overflow-hidden rounded-xl p-4 shadow-slate-900/50">
+              <div className="flex flex-col  h-full items-center w-full  text-2xl ">
+                <Image
+                  src={ourData[index].image}
+                  alt="dj"
+                  className="h-36 aspect-square w-36 rounded-full object-cover "
+                  width={400}
+                  height={400}
+                />
+                <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 font-bold">{ourData[index].name}</h1>
+                <p className="text-sm text-slate-400">
+                  {ourData[index].bio}
+                </p>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Introduction;
--- src/libs/data.ts
+++ src/libs/data.ts
@@ -1,25 +1,70 @@
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 type OurData = {
     name: string;
     image: string;
     // skills: string;
     bio: string;
-    // socialLinks: {
-    //     name: string;
-    //     link: string;
-    // };
+    socialLinks: {
+        name: string;
+        link: string;
+        logo: any;
+    }[]
 }[]
 
 export const ourData: OurData = [
     {
         name: "Md Murad",
-        image: "../../public/images/murad.jpg",
+        image: "/images/murad.jpg",
         bio: `(CSE)💻
         My dream is to become a MERN stack developer
-        alert('😈');`
-    },
+        alert('😈');`,
+        socialLinks: [{
+            name: "Github",
+            link: "https://github.com/nifad2005",
+            logo: FaGithub
+        }, {
+            name: "LinkedIn",    
+            link: "https://www.linkedin.com/in/nifad2005/",
+            logo: FaLinkedin
+        },{
+            name: "Twitter",
+            link: "https://twitter.com/nifad2005",
+            logo: FaSquareXTwitter 
+        },
+        {
+            name: "Facebook",
+            link: "https://www.facebook.com/nifad2005",
+            logo: FaFacebook
+        }
+    ],
+    },   
     {
         name: "Md Nifad Uzzaman",
-        image: "../../public/images/murad.jpg",
-        bio: "WebDeveloper | BookLover | PassionateTraveler | Resilient | SpirituallyGrounded"
+        image: "/images/nifad.jpg",
+        bio: "WebDeveloper | BookLover | PassionateTraveler | Resilient | SpirituallyGrounded",
+        socialLinks: [{
+            name: "Github",
+            link: "https://github.com/nifad2005",
+            logo: FaGithub
+        }, {
+            name: "LinkedIn",    
+            link: "https://www.linkedin.com/in/nifad2005/",
+            logo: FaLinkedin
+        },{
+            name: "Twitter",
+            link: "https://twitter.com/nifad2005",
+            logo: FaSquareXTwitter
+        },
+        {
+            name: "Facebook",
+            link: "https://www.facebook.com/nifad2005",
+            logo: FaFacebook
+        }
+        
+    ]
     }
 ]
